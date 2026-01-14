98418844c63ff5aefd27cb37fe9b9ca1a7f9a9e8
Sky <sky@leite.dev>
2026-01-14 10:47:43 -0300

    Add hbnix cachix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,14 @@
 {
   description = "VitaSDK core & libraries packaged for Nix";
+  nixConfig = {
+    extra-substituters = [
+      "https://hbnix.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "hbnix.cachix.org-1:KKKoJ7QLIYjMC1CRLFgeTwfO9bKzSgu2G8BbyZQIe/M="
+    ];
+  };
+
   outputs = { self, nixpkgs }: rec {
     packages = builtins.mapAttrs (k: v: import ./vitasdk-packages.nix { pkgs = v; }) nixpkgs.legacyPackages;
     defaultPackage = builtins.mapAttrs (k: v: v.vitasdk) packages;
